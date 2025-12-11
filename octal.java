8d8b25fa4c6f47ad97fbff04233241d363c6e5e8
Nivetha003 <niviragu2k5@gmail.com>
2025-12-11 22:38:26 +0530

    Add files via upload

---
+++ octal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class octal {
+     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("octal number: %o",a);
+              //  int num = 255;  
+
+        //System.out.printf("Hexadecimal: %X", num);
+
+    }
+}
