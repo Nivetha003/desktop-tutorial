21fb036d5a5728904c6ef2f75eaa5a0ddb9e9e35
Nivetha003 <niviragu2k5@gmail.com>
2025-12-11 22:42:01 +0530

    Add files via upload

---
+++ char.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class char {
+        public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       Char ch= sc.next().CharAt(0);
+       System.out.println();
+    }
+}
