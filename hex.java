84bb20a276cf5f33cbf3bafa4942837fe9b21f05
Nivetha003 <niviragu2k5@gmail.com>
2025-12-11 22:39:50 +0530

    Add files via upload

---
+++ hex.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hex {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("hexadecimal  number: %X",a);
+              //  int num = 255;  
+
+        //System.out.printf("Hexadecimal: %X", num);
+
+    }
+}
