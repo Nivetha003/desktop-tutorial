02b4847b039e889a7fea2c1a33b5d271a1691a9b
Nivetha003 <niviragu2k5@gmail.com>
2025-12-11 22:40:14 +0530

    Add files via upload

---
+++ fact2d.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class fact2d {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.printf("Formatted  number: %.3f",a);
+    }
+}
+
+ 
