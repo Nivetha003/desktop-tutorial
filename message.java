7c58a2d8d97fa407110704d2de6f1003c1d93300
Nivetha003 <niviragu2k5@gmail.com>
2025-12-11 22:39:19 +0530

    Add files via upload

---
+++ message.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class message {
+    public static void main(String[] args) {
+      Scanner msg = new Scanner(System.in);
+       String line= msg.nextLine();
+       System.out.println(line);
+    }
+}
