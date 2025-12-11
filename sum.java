2a784e759c15dd941d78554e4c1408f25580388a
Nivetha003 <niviragu2k5@gmail.com>
2025-12-11 19:59:17 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+       // Scanner sc = new Scanner(System.in);
+         int a =10;
+         int b =14;
+         System.out.println(a+b);
+    }
+}
